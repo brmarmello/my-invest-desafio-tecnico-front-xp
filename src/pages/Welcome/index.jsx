f707253f6e73a3c09bab682e4eff55c2bb9add2c
brmarmello <brmarmello@gmail.com>
2022-07-17 18:42:01 -0300

    feat: ends animations on the Welcome screen.

--- src/pages/Welcome/index.jsx
+++ src/pages/Welcome/index.jsx
@@ -14,14 +14,15 @@ export default function Welcome() {
     <View style={styles.container}>
       
       <View style={styles.containerLogo}>
-        <Image
+        <Animatable.Image
+          animation="flipInY"
           source={require('../../assets/logo-xp-inc.png')}
           style={{ width: '100%' }}
           resizeMode="contain"
         />
       </View>
 
-      <View style={styles.containerForm}>
+      <Animatable.View delay={600} animation="fadeInUp" style={styles.containerForm}>
         <Text style={styles.textTitle}>Processo Seletivo para Mobile</Text>
         <Text style={styles.textDev}>Desenvolvido por Bruno Marmello</Text>
         <Text style={styles.textLogin}>Faça Login Para Começar!</Text>
@@ -29,7 +30,7 @@ export default function Welcome() {
         <TouchableOpacity style={styles.button}>
           <Text style={styles.buttonText}>Acessar</Text>
         </TouchableOpacity>
-      </View>
+      </Animatable.View>
 
     </View>
   );
