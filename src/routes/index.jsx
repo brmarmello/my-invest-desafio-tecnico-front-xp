9fd94598e1c4090e121b9f7e365efec2f150eb8d
brmarmello <brmarmello@gmail.com>
2022-07-17 15:58:43 -0300

    dev: create route configuration for existing pages.

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -4,3 +4,19 @@ import Welcome from '../pages/Welcome';
 import SignIn from '../pages/SignIn';
 
 const Stack = createNativeStackNavigator();
+
+export default function Routes() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Welcome"
+        component={Welcome}
+      />
+
+      <Stack.Screen
+        name="SignIn"
+        component={SignIn}
+      />
+    </Stack.Navigator>
+  );
+}
