9ea44ad333a6222216f3f7de35142adaf18c4a02
brmarmello <brmarmello@gmail.com>
2022-07-17 17:17:55 -0300

    dev: upload the logo in the app.

--- src/pages/Welcome/index.jsx
+++ src/pages/Welcome/index.jsx
@@ -11,6 +11,14 @@ export default function Welcome() {
   return (
     <View style={styles.container}>
       
+      <View style={styles.containerLogo}>
+        <Image
+          source={require('../../assets/logo-xp-inc.png')}
+          style={{ width: '100%' }}
+          resizeMode="contain"
+        />
+      </View>
+
     </View>
   );
 }
