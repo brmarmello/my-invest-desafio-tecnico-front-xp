a413f5f3e9e789fa7a9006dad725e07e69339d4a
brmarmello <brmarmello@gmail.com>
2022-07-17 16:26:28 -0300

    dev: assembles style structure for Welcome page.

--- src/pages/Welcome/index.jsx
+++ src/pages/Welcome/index.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+} from 'react-native';
 
 export default function Welcome() {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Tela Boas Vindas</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    
+  }
+})
