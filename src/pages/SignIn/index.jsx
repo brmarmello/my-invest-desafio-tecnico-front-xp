a1e03942d2a178e7206d4c9532383da944ef7c27
brmarmello <brmarmello@gmail.com>
2022-07-17 19:57:57 -0300

    feat: finalizes Login screen content and style.

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
 
 import * as Animatable from 'react-native-animatable';
 
@@ -8,13 +14,93 @@ import * as Animatable from 'react-native-animatable';
 export default function SignIn() {
   return (
     <View style={styles.container}>
-      <Text>Tela Login</Text>
+
+      <Animatable.View animation="fadeInLeft" delay={500} style={styles.containerHeader}>
+        <Text style={styles.message}>Bem-Vinda || Bem-Vindo</Text>
+      </Animatable.View>
+
+      <Animatable.View animation="fadeInUp" style={styles.containerForm}>
+        <Text style={styles.textEmail}>e-Mail:</Text>
+        <TextInput
+          placeholder="Digite seu Melhor e-Mail..."
+          style={styles.input}
+        />
+
+        <Text style={styles.textEmail}>Senha:</Text>
+        <TextInput
+          placeholder="Digite sua Senha..."
+          style={styles.input}
+        />
+
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.buttonRegister}>
+          <Text style={styles.registerText}>Não Possui uma Conta? Cadastre-se!</Text>
+        </TouchableOpacity>
+      </Animatable.View>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-
+    flex: 1,
+    backgroundColor: '#000000',
+  },
+  containerHeader: {
+    marginTop: '14%',
+    marginBottom: '8%',
+    paddingStart: '1%',
+  },
+  message: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#EFEEEE',
+    textAlign: 'center',
+  },
+  containerForm: {
+    backgroundColor: '#EFEEEE',
+    flex: 1,
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    paddingStart: '5%',
+    paddingEnd: '5%',
+  },
+  textEmail: {
+    fontSize: 24,
+    marginTop: 28,
+    fontWeight: 'bold',
+  },
+  input: {
+    borderBottomWidth: 1,
+    height: 40,
+    marginBottom: 12,
+    fontSize: 16,
+  },
+  button: {
+    backgroundColor: '#FFC708',
+    width: '100%',
+    borderRadius: 4,
+    paddingVertical: 8,
+    marginTop: 14,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#767e67',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  buttonRegister: {
+    marginTop: 14,
+    alignSelf: 'center',
+  },
+  registerText: {
+    color: '#767e67',
+    fontSize: 15,
+    fontWeight: 'bold',
   }
 })
