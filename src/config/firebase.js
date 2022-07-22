1df9dc59d09f0603100385aeb048dca45117c0fa
brmarmello <brmarmello@gmail.com>
2022-07-22 19:28:09 -0300

    dev: install firebase and start authentication setup.

---
+++ src/config/firebase.js
@@ -0,0 +1,23 @@
+import firebase from "firebase";
+import 'firebase/storage';
+
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.1/firebase-app.js";
+// import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.9.1/firebase-analytics.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDLoO3g7ONiSeF8UHGhsql5hDaIa5_qVU8",
+    authDomain: "my-invest-89f25.firebaseapp.com",
+    projectId: "my-invest-89f25",
+    storageBucket: "my-invest-89f25.appspot.com",
+    messagingSenderId: "697215775838",
+    appId: "1:697215775838:web:975bf43ad9d8c39b64f0cf",
+    measurementId: "G-YEDG0V4SX5"
+};
+
+firebase.initializeApp(firebaseConfig);
+const database = firebase.firestore();
+
+export default database;
+  
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
