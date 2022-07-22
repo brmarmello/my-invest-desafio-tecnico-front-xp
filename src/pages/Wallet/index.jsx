0bc2ab4a5c236818c0a26505a8ac7160c9ab917b
brmarmello <brmarmello@gmail.com>
2022-07-22 12:57:48 -0300

    fix: import navigation and animations.

--- src/pages/Wallet/index.jsx
+++ src/pages/Wallet/index.jsx
@@ -8,6 +8,10 @@ import {
   TouchableOpacity,
 } from 'react-native';
 
+import * as Animatable from 'react-native-animatable';
+
+import { useNavigation } from '@react-navigation/native';
+
 // import {
 //   Table,
 //   TableWrapper,
